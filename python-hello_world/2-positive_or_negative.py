77680a76be363a794a07336e21254123970cfc69
root <you@example.com>
2023-07-16 09:32:23 -0700

    positive negative or zero

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, "is positive", end="")
+    print(number, "is positive")
 elif number == 0:
-    print(number, "is zero", end="")
+    print(number, "is zero")
 else:
-    print(number, "is negative", end="")
+    print(number, "is negative")
