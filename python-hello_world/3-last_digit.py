34a8ba5cc4b9a5b868afda22a00395b7635106ea
root <you@example.com>
2023-07-16 08:01:46 -0700

    checking the last digit of a random number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,11 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-print("Last digit of", number, "is",last_digit, end="")
+prefix = "Last digit of {} is".format(number)
 
-if last_digit > 5:
-    print(" and is greater than 5")
+if number <  0:
+    last_digit = -last_digit
+    print("{} {} and is less than 6 and not 0".format(prefix, last_digit))
+elif last_digit > 5:
+    print("{} {} and is greater than 5".format(prefix, last_digit))
 elif last_digit == 0:
-    print(" and is 0")
+    print("{} {} and is 0".format(prefix, last_digit))
 else:
-    print(" and is less than 6 and not 0")
+    print("{} {} and is less than 6 and not 0".format(prefix, last_digit))
