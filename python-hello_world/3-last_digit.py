44508171258471f012612547db4fe8ba79835a03
root <you@example.com>
2023-07-16 08:03:28 -0700

    checking the last digit of a random number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 prefix = "Last digit of {} is".format(number)
 
-if number <  0:
+if number < 0:
     last_digit = -last_digit
     print("{} {} and is less than 6 and not 0".format(prefix, last_digit))
 elif last_digit > 5:
