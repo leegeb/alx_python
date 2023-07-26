f51eceec2866ea0b4d7cac10b045e854d9d0693e
Lee <111172410+leegeb@users.noreply.github.com>
2023-07-26 04:23:58 +0400

    print division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -10,6 +10,6 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result) if result is not None else "Inside result: None")
-    return result
+  
 
 
