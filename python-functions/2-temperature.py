30a282766f258030dd58033ff478e8a57cb4acaf
Lee <111172410+leegeb@users.noreply.github.com>
2023-07-26 20:24:17 +0400

    Delete python-functions directory

--- python-functions/2-temperature.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
--- python-functions/3-string.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def reverse_string(string):
-    reversed_string = ""
-    for char in string[::-1]:
-        reversed_string += char
-        return reversed_string
