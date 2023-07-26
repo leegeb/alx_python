221cabbd02f29b6179a828c015c869371f3cfc35
Lee <111172410+leegeb@users.noreply.github.com>
2023-07-26 22:58:30 +0400

    Update 1-common_elements.py

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_elements_set = set()
-
-    for element in set_1:
-        if element in set_2:
-            common_elements_set.add(element)
-
-    return common_elements_set
+    common_set = set_1 & set_2
+    return common_set
