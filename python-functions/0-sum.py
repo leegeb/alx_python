17b2005a1ed09e8f994363d9956a9b35006b980c
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-07 00:43:23 +0400

    Delete python-functions directory

--- python-functions/0-sum.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
-
-    return a
--- python-functions/3-string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def reverse_string(string):
-    reversed_string = string[::-1]
-    return reversed_string
