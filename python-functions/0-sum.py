1c48dcfb3db56ad15b79fe52ad94dfc64ce008fb
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-07 00:48:43 +0400

    Update 0-sum.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
 def add(a, b):
- while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
-    return a
+    if b == 0:
+        return a
+    else:
+        return add(a ^ b, (a & b) << 1)
