97b887eb2b3188b08cc6dff0f748a00da10154de
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-11 01:48:08 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python3
 class Square:
     """
-    This class defines a square.
+    A class representing a square.
 
     Attributes:
-        __size (int): The size of the square's side (private).
-    """
+        __size (int): The size of the square's sides (private).
 
+    Methods:
+        __init__(self, size=0): Constructor method for initializing the square.
+        area(self): Calculates and returns the area of the square.
+    """
+    
     def __init__(self, size=0):
         """
-        Initializes a Square instance.
+        Initializes a square with the given size.
 
         Args:
-            size (int, optional): The size of the square's side (default is 0).
-                It must be a non-negative integer.
-
+            size (int, optional): The size of the square's sides. Defaults to 0.
+        
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
@@ -23,8 +25,9 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        
         self.__size = size
-
+    
     def area(self):
         """
         Calculates and returns the area of the square.
@@ -33,3 +36,4 @@ class Square:
             int: The area of the square.
         """
         return self.__size ** 2
+
