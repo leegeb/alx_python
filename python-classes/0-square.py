a5d2d68c70b22de8fc4a48a846294f48e6df4871
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-11 01:40:50 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 class Square:
     """
-    This class defines a square.
+    A class representing a square.
 
     Attributes:
-        __size (int): The size of the square's side (private).
-    """
+        __size (int): The size of the square's sides (private).
 
+    Methods:
+        __init__(self, size): Constructor method for initializing the square.
+    """
+    
     def __init__(self, size):
         """
-        Initializes a Square instance.
+        Initializes a square with the given size.
 
         Args:
-            size (int): The size of the square's side.
+            size (int): The size of the square's sides.
         """
         self.__size = size
