a8cd5c3120ec61a5259ef6188dcd6bd3d4a06dcf
lydia gebre <lydiakgebre@gmail.com>
2023-08-12 02:31:41 +0400

    Rectangle-class-Fix-Six

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -42,15 +42,6 @@ class Rectangle(BaseGeometry):
         self.integer_validator("width", self.__width)
         self.integer_validator("height", self.__height)
 
-
-r = Rectangle(3, 5)
-print(r)
-print(dir(r))
-try:
-    print("Rectangle: {} - {}".format(r._Rectangle__width, r._Rectangle__height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def __str__(self):
+        """Return a string representation of the object"""
+        return f"Rectangle: {self.__width} - {self.__height}"
