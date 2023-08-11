586c48375d5231164f4b941e3b038c827f712d6c
lydia gebre <lydiakgebre@gmail.com>
2023-08-12 00:08:45 +0400

    python-inheritance-two

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,4 +2,5 @@
 
 
 def inherits_from(obj, a_class):
-    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
+    """returns true/false"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
