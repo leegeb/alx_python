af9afd8cba893de42fd8aa5a233b50f12f57a2a9
lydia gebre <lydiakgebre@gmail.com>
2023-08-12 01:24:28 +0400

    class BaseGeometry - four

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,22 @@
+"""removes the __init__subclass__"""
+
+
+class ExcludeInitSubclassMeta(type):
+    """Remove __init_subclass__"""
+    def __dir__(cls):
+        attributes = super().__dir__()
+        return [attr for attr in attributes if attr != "__init_subclass__"]
+
+
+"""empty class"""
+
+
+class BaseGeometry(metaclass=ExcludeInitSubclassMeta):
+    """Empty class with __init_subclass__ removed"""
+
+    def __dir__(self):
+        original_attrs = super().__dir__()
+        return [attr for attr in original_attrs if attr != "__init_subclass__"]
+
+    def area(self):
+        raise Exception("area() is not implemented")
