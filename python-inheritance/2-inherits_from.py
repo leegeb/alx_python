b733c2b1f9b8d620a1bfeb95ca06e33c76717cb0
lydia gebre <lydiakgebre@gmail.com>
2023-08-11 23:59:08 +0400

    python-inheritance-two

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,5 @@
+"""function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
