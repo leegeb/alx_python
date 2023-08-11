6b410c7838aab246aed21afb69956f6f8f6aca65
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-12 00:10:46 +0400

    function module added 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,4 +2,5 @@
 
 
 def inherits_from(obj, a_class):
+    """True if the object's class hierarchy includes a_class; False otherwise."""
     return (issubclass(type(obj), a_class) and type(obj) is not a_class)
