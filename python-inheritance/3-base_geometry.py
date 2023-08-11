2abfb035131e3619b505564bc56a0fb3c5d77b78
lydia gebre <lydiakgebre@gmail.com>
2023-08-12 00:56:10 +0400

    empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,6 @@
-class ExcludeInitSubclassMeta(type):
-    """Remove __init_subclass__"""
-    def __dir__(cls):
-        attributes = super().__dir__()
-        return [attr for attr in attributes if attr != "__init_subclass__"]
+"""creating an empty class"""
 
 
-class BaseGeometry(metaclass=ExcludeInitSubclassMeta):
-    """Empty class with __init_subclass__ removed"""
+class BaseGeometry:
+    """Empty class"""
     pass
