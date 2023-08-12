24bf55d3c229303ff7456ade80f31eb782f6a04b
lydia gebre <lydiakgebre@gmail.com>
2023-08-12 04:19:55 +0400

    Adding test files -Rectangle class

--- python-inheritance/test-rectangle.py
+++ python-inheritance/test-rectangle.py
@@ -3,20 +3,20 @@
 
 class BaseGeometry:
     """the empty class"""
-    def __dir__(cls) -> None:
+
+    def __dir__(self):
         attributes = super().__dir__()
         n_attributes = []
         for attr in attributes:
             if attr != "__init_subclass__":
                 n_attributes.append(attr)
-        attributes = n_attributes
-        return attributes
+        return n_attributes
 
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """this validaes value"""
+        """this validates value"""
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value < 0 or value == 0:
