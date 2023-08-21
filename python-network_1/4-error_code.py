1ca797a35a4ad34c2832b2c4b96a21f545d4d4ac
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-22 03:12:25 +0400

    error_code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,11 @@
+"""script that takes in a URL, sends a request to the URL and displays the body of the response."""
+import requests
+import sys
+
+url = sys.argv[1]
+
+response = requests.get(url)
+print(response.text)
+
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
