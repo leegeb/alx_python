7f03cfc61dcffdd37e6ba1667b88c27418e87cfe
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-22 03:04:17 +0400

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,24 +1,15 @@
+"""script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header"""
 """importing requests"""
+
+"""importing sys"""
+
 import requests
+import sys
+url = sys.argv[1]
 
 
-def main():
-
-    url = input("Enter a URL: ")
-
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        x_request_id = response.headers.get('X-Request-Id')
-
-        if x_request_id:
-            print("X-Request-Id:", x_request_id)
-        else:
-            print("X-Request-Id not found in response headers")
-    else:
-        print("Error: Unable to fetch data from the URL. Status code:",
-              response.status_code)
-
-
-if __name__ == "__main__":
-    main()
+request = requests.get(url)
+try:
+    print(request.headers['X-Request-Id'])
+except:
+    pass
