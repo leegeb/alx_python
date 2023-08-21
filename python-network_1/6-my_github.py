5292321eea0ae6193fc06270b0b050fe991a1083
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-22 03:52:42 +0400

    Update 6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,11 +1,15 @@
 """my github"""
 import requests
 import sys
-import os
 
 username = sys.argv[1]
-token = os.environ.get("GITHUB_TOKEN")
+token = sys.argv[2]
 
 response = requests.get(f"https://api.github.com/users/{username}", auth=(username, token))
 
-print(response.json().get('id', 'Error'))
+if response.status_code == 200:
+    user_data = response.json()
+    print(user_data['id'])
+else:
+    print(f"Error: {response.status_code}")
+
