9b17d1bea7249c8969052ef9e25d4389fa98e1c1
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-24 23:19:43 +0400

    python-object_relational_mapping

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first = session.query(State).order_by(State.id).first()
+
+    if first:
+        print("{}: {}".format(first.id, first.name))
+    else:
+        print("Nothing")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
