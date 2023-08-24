13c920559e03ae165e36793d40c65e136c5a6675
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-24 23:06:22 +0400

    python-object_relational_mapping

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+"""connect to a MySQL database.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
