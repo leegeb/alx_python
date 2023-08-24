6bb885bbce7409764de63330c6d8278ec0923b61
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-24 21:53:50 +0400

    py-object-relation-mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,18 @@
 import sys
+import MySQLdb as mdb
 
+def main():
+    database = mdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = database.cursor()
 
-class FakeMySQLCursor:
-    def execute(self, query):
-        self.results = [(1, 'State 1'), (2, 'State 2'), (3, 'State 3')]
+    cur.execute("SELECT * FROM states ORDER BY id")
+    results = cur.fetchall()
 
-    def fetchall(self):
-        return self.results
+    for result in results:
+        print(result)
 
+    cur.close()
+    database.close()
 
-cur = FakeMySQLCursor()
-cur.execute("SELECT * FROM states ORDER BY id")
-results = cur.fetchall()
-
-for result in results:
-    print(result)
+if __name__ == "__main__":
+    main()
