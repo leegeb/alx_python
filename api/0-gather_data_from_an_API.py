8a5b180a14941bfe422a64174154c38660969418
Lee <111172410+leegeb@users.noreply.github.com>
2023-10-12 14:51:21 +0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,12 +14,14 @@ def get_employee_todo_progress(employee_id):
 
     # Extract relevant information
     employee_name = user_data["name"]
+    # Limit employee name to 18 characters
+    truncated_name = employee_name[:18] if len(employee_name) > 18 else employee_name
     total_tasks = len(todo_data)
     completed_tasks = [task for task in todo_data if task["completed"]]
     number_of_done_tasks = len(completed_tasks)
 
     # Print the required output format
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {truncated_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
@@ -32,10 +34,4 @@ if __name__ == "__main__":
             get_employee_todo_progress(employee_id)
         except ValueError:
             print("Invalid employee ID. Please provide a valid integer.")
- script.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_todo_progress(employee_id)
-        except ValueError:
-            print("Invalid employee ID. Please provide a valid integer.")
+
