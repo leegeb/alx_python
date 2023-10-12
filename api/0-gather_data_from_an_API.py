d63fc4c7fd4f51e49ba696f801abe19d94f840b3
Lee <111172410+leegeb@users.noreply.github.com>
2023-10-12 14:49:18 +0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,29 +2,37 @@
 import requests
 import sys
 
-
 def get_employee_todo_progress(employee_id):
-    employee_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    employee_data = employee_response.json()
-    employee_name = employee_data['name']
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 
-    todos_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    todos_data = todos_response.json()
+    todo_response = requests.get(todo_url)
+    user_response = requests.get(user_url)
 
-    completed_tasks = [todo for todo in todos_data if todo['completed']]
+    todo_data = todo_response.json()
+    user_data = user_response.json()
+
+    # Extract relevant information
+    employee_name = user_data["name"]
+    total_tasks = len(todo_data)
+    completed_tasks = [task for task in todo_data if task["completed"]]
     number_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todos_data)
 
-    print(
-        f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
+    # Print the required output format
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
-        if __name__ == "__main__":
-            if len(sys.argv) != 2:
-                print("Usage: python script.py <employee_id>")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            get_employee_todo_progress(employee_id)
+        except ValueError:
+            print("Invalid employee ID. Please provide a valid integer.")
+ script.py <employee_id>")
     else:
         try:
             employee_id = int(sys.argv[1])
