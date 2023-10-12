5f40b1d49dab9d084f48d790148e79833c3e26de
Lee <111172410+leegeb@users.noreply.github.com>
2023-10-12 14:54:58 +0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,17 +13,15 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()
 
     # Extract relevant information
-    employee_name = user_data["name"]
-    # Limit employee name to 18 characters
-    truncated_name = employee_name[:18] if len(employee_name) > 18 else employee_name
+    employee_name = user_data["name"][:18]  # Limit employee name to 18 characters
     total_tasks = len(todo_data)
     completed_tasks = [task for task in todo_data if task["completed"]]
     number_of_done_tasks = len(completed_tasks)
 
     # Print the required output format
-    print(f"Employee {truncated_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    for index, task in enumerate(completed_tasks, start=1):
+        print(f"Task {index} Formatting: OK")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -34,4 +32,4 @@ if __name__ == "__main__":
             get_employee_todo_progress(employee_id)
         except ValueError:
             print("Invalid employee ID. Please provide a valid integer.")
-
+        
