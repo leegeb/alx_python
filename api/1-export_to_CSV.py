968d2a0280ab8162d88ecff8fe45ed8a88658f62
Lee <111172410+leegeb@users.noreply.github.com>
2023-10-13 10:39:23 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
+import csv
 import requests
 import sys
-import csv
 
+if __name__=="__main__":
+    
+    user_id = sys.argv[1]
 
-def get_employee_data(employee_id):
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    user_id = user_data.get('id')
-    username = user_data.get('username')
 
-    todo_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
     todo_response = requests.get(todo_url)
     todo_data = todo_response.json()
+    
 
-    csv_data = []
-    for task in todo_data:
-        task_completed_status = "Completed" if task['completed'] else "Not Completed"
-        task_title = task['title']
-        csv_data.append([user_id, username, task_completed_status, task_title])
+    csv_file = "{}.csv".format(user_id)
 
-    with open(f'{user_id}.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-        writer.writerows(csv_data)
-
-    print(f'CSV file "{user_id}.csv" has been created successfully.')
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_data(employee_id)
-        except ValueError:
-            print("Invalid employee ID. Please provide a valid integer.")
+    with open(csv_file, 'w') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for task in todo_data:
+            csv_writer.writerow([user_id, str(user_data['username']), task['completed'], task['title']])
+        csvfile.close()
